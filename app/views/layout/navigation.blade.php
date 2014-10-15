daa1905247aee9169c727cd9c2e99e5dbcfbcfc7
saqibbaig <saqibbaig@gmail.com>
2014-10-15 06:22:45 +0100

    Menu option added

--- app/views/layout/navigation.blade.php
+++ app/views/layout/navigation.blade.php
@@ -4,8 +4,17 @@
 	@if(Auth::check())
 	@if(Auth::user()->acc_type==1)
 	Administrator Menu 
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Admin Option 1 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Admin Option 2 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Admin Option 3 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Admin Option 4 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Admin Option 5</A></li>
 	@elseif((Auth::user()->acc_type)==2)
 	Manager Menu
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Manager Option 1 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Manager Option 2 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Manager Option 3 </A></li>
+	<li><A href="{{ URL::route('account-updateprofile') }}"> Manager Option 4</A></li>
 
 	@else
 	User Menu
